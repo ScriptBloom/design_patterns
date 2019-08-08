09987769e8befd9981f59fbed9480ab0a16122c0
ding-zou <1774113002@qq.com>
2019-08-08 12:58:21 +0800

    工厂模式

--- factory_patterns/src/main/java/top/dzou/factory_patterns/abstract_factory_pattern/ingredient/LASauce.java
+++ factory_patterns/src/main/java/top/dzou/factory_patterns/abstract_factory_pattern/ingredient/LASauce.java
@@ -7,6 +7,7 @@ import top.dzou.factory_patterns.abstract_factory_pattern.Sauce;
  * @date 19-8-6 上午11:06
  */
 public class LASauce extends Sauce {
+
     public LASauce(){
         sauceName = "LA sauce";
     }
